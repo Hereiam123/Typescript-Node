8b3f3e8b47f098979fea5ef35ac80b4c539f6ead
Brian De Maio <bdemaio1@gmail.com>
2020-08-11 01:23:34 -0400

    Simple todo patch route setup

--- src/controllers/todos.ts
+++ src/controllers/todos.ts
@@ -15,3 +15,16 @@ export const createTodo: RequestHandler = (req, res, next) => {
 export const getTodos: RequestHandler = (req, res, next) => {
   res.status(200).json({ todos: TODOS });
 };
+
+export const updateTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId = req.params.id;
+  const newText = (req.body as { text: string }).text;
+  const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+  if (todoIndex < 0) {
+    throw new Error("Could not find todo!");
+  }
+
+  TODOS[todoIndex] = new Todo(TODOS[todoIndex].id, newText);
+
+  res.json({ message: "Todo updated", todoUpdate: TODOS[todoIndex] });
+};
